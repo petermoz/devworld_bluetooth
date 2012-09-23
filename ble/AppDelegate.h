73aba8972240679557d29c70611ee32e3b69c2e7
Peter Morton <petermorton@gmail.com>
2012-09-23 16:15:44 +1000

    Initial Commit

---
+++ ble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ble
+//
+//  Created by Peter Morton on 23/09/12.
+//  Copyright (c) 2012 Peter Morton. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ble/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ble
+//
+//  Created by Peter Morton on 23/09/12.
+//  Copyright (c) 2012 Peter Morton. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
