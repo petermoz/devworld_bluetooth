7778517a8c7eaa468c591307a290cc15a3048aa4
Peter Morton <petermorton@gmail.com>
2012-09-23 16:28:22 +1000

    Basic Bluetooth LE state display

--- ble/AppDelegate.m
+++ ble/AppDelegate.m
@@ -7,12 +7,48 @@
 //
 
 #import "AppDelegate.h"
+#import <IOBluetooth/IOBluetooth.h>
+
+@interface AppDelegate () <CBCentralManagerDelegate>
+
+@property (nonatomic, retain) CBCentralManager *manager;
+@property (nonatomic, retain) NSDictionary *known_uuids;
+
+@end
+
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    self.known_uuids = @{
+        [CBUUID UUIDWithString:@"03879136-EAA2-47A1-BA01-936BA9B86F3C"] : @"Peter",
+        [CBUUID UUIDWithString:@"E70A9276-AEC3-47A5-BA43-C1A8C803DF35"] : @"Pi"};
+    
+    self.manager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
 }
+
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    switch(central.state)
+    {
+        case CBCentralManagerStatePoweredOff:
+            self.message.stringValue = @"Bluetooth is powered off";
+            break;
+            
+        case CBCentralManagerStatePoweredOn:
+            self.message.stringValue = @"Bluetooth is powered on";
+            break;
+            
+        case CBCentralManagerStateUnsupported:
+        case CBCentralManagerStateUnknown:
+        case CBCentralManagerStateUnauthorized:
+            self.message.stringValue = @"Problem initialising";
+    }
+    
+}
+
+
 
 @end
